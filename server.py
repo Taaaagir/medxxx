4374ceef4dd101ea3c087535b744fec7b59bbc58
Tagir <166266932+Taaaagir@users.noreply.github.com>
2026-03-29 17:41:53 +0400

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ import asyncio
 from create_bot import dp, bot
 
 async def main():
+    # 💥 КЛЮЧЕВАЯ СТРОКА — убирает конфликт
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    # запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
