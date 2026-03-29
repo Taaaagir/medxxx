77982b7196b5de8ad54401ea8b4adfebdeda08d8
Tagir <166266932+Taaaagir@users.noreply.github.com>
2026-03-29 17:37:23 +0400

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-BOT_TOKEN = "ТВОЙ_ТОКЕН"
+BOT_TOKEN = "8063265091:AAESmXvjW1z3esWyILITzJTxqemKnAdT9nk"
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
