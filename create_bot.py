101a1e1e859492a3cf95bb0016b8d906da213fc9
Tagir <166266932+Taaaagir@users.noreply.github.com>
2026-03-29 17:36:22 +0400

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,19 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-BOT_TOKEN = "8063265091:AAESmXvjW1z3esWyILITzJTxqemKnAdT9nk"
+BOT_TOKEN = "ТВОЙ_ТОКЕН"
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
+
+async def main():
+    # 💥 УБИРАЕТ КОНФЛИКТ (ОЧЕНЬ ВАЖНО)
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    # 🚀 запуск бота
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
