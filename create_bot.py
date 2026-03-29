3f3b07ab9c7b8bc19058b16269e26d87e7b1b299
Tagir <166266932+Taaaagir@users.noreply.github.com>
2026-03-29 17:55:41 +0400

    Add router to the bot dispatcher
    Included router from start module to the dispatcher.

--- create_bot.py
+++ create_bot.py
@@ -1,19 +1,11 @@
-import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from start import router
 
 BOT_TOKEN = "8063265091:AAESmXvjW1z3esWyILITzJTxqemKnAdT9nk"
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
-async def main():
-    # 💥 УБИРАЕТ КОНФЛИКТ (ОЧЕНЬ ВАЖНО)
-    await bot.delete_webhook(drop_pending_updates=True)
-
-    # 🚀 запуск бота
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+dp.include_router(router)
